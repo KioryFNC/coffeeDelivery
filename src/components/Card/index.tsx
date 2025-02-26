1be40095294ad243570008862d485eb77555cbc4
yuri <yuri.barros.luz.familia@gmail.com>
2025-02-26 12:01:35 -0300

    feat: initial Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,25 @@
 import * as Element from './styles'
 
-export function Card() {
+interface CardProps {
+  image: string;
+  tags: string[];
+  title: string;
+  description: string;
+  price: number;
+}
+
+export function Card({ image, tags, title, description, price}: CardProps) {
   return (
     <Element.Root>
-      
+      <Element.Image src={image} alt={title}/>
+      <Element.Tags>
+        {tags.map((tag, index) => (
+          <Element.Tag key={index}>{tag}</Element.Tag>
+        ))}
+      </Element.Tags>
+      <Element.Title>{title}</Element.Title>
+      <Element.Description>{description}</Element.Description>
+      <Element.Price>R$ {price.toFixed(2)}</Element.Price>
     </Element.Root>
   )
 }
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -8,3 +8,29 @@ export const Root = styled.div`
   justify-content: center;
   flex-direction: column;
 `
+
+export const Image = styled.img`
+  width: 12rem;
+  height: 12rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const Tags = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: .4rem .8rem;
+  background: ${(props) => props.theme['yellow-light']};
+  border-radius: 8px;
+`
+export const Tag = styled.span`
+  font-size: 1rem;
+  font-weight: 700;
+  line-height: 130%;
+  text-transform: uppercase;
+  color: ${(props) => props.theme['yellow-dark']};
+`
+export const Title = styled.h3``
+export const Description = styled.p``
+export const Price = styled.span``
